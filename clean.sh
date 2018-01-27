385a32aa5155368be9219386b5c7f98d5a15f47a
Mtronix <mike@mtronix.pl>
2018-01-27 17:23:24 +0100

    Move logic of creating CNAME file to .travis.yml

--- clean.sh
+++ clean.sh
@@ -3,8 +3,8 @@
 # Cleans all unnecessary files after the clone repository to start creating a new website based on this project
 # You can run this script only one time, because it is self-destroying after project cleanup
 
-# Remove TravisCI configuration file, CNAME and .git directory
-rm -rf .travis.yml CNAME .git/
+# Remove TravisCI configuration file and .git directory
+rm -rf .travis.yml .git/
 
 # Self destroy
 rm -- "$0"
