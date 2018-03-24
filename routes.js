2abc5b5e4e98b5ca50e01a3834bc28e4b50d2e17
Mtronix <mike@mtronix.pl>
2018-03-24 11:17:44 +0100

    Bump version of libraries

---
+++ routes.js
@@ -0,0 +1,21 @@
+const routes = [
+  {
+    name: 'index.pug',
+    title: 'Main page',
+    url: 'index.html',
+    data: {
+      posts: [1, 2, 3, 4, 5]
+    }
+  },
+  {
+    name: 'simple-page.pug',
+    title: 'Simple page',
+    url: 'simple-page.html',
+    data: {}
+  },
+  {},
+  {},
+  {}
+];
+
+export default routes;
